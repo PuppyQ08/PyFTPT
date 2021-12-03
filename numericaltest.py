7d9bfdcdb835818878054fbc63cef664d67dafa6
xiuyiqin <terryqqy@gmail.com>
2021-12-03 00:51:35 -0600

    final bug

--- numericaltest.py
+++ numericaltest.py
@@ -93,12 +93,12 @@ class Numerical:
 
                     D13 += -FCQ4[i,i,j,j]**2*(2*f[i]**2*f[j] + f[i]**2 + 2*f[i]*f[j]**2 + 4*f[i]*f[j] + 2*f[i] + f[j]**2 + 2*f[j] + 1)/(8*(w[i] + w[j]))/2#XXX 6*6 ???
                     D14 +=  FCQ4[i,i,j,j]**2*(f[i]**2 + 2*f[i]*f[j]*(f[i] - f[j]) - f[j]**2)/(8*(w[i] - w[j]))/2# 6*6???
-                    D15 += -FCQ4[i,j,j,j]**2*(f[i] + 12*f[j]**2 + 6*f[j]*(f[i]*f[j] + f[i] + f[j]**2) + 7*f[j] + 1)/(8*(w[i] + w[j]))#4*4
-                    D16 +=  FCQ4[i,j,j,j]**2*(f[i] + 6*f[j]*(f[i]*f[j] + f[i] - f[j]**2 - f[j]) - f[j])/(8*(w[i] - w[j]))#4*4
+                    D15 += -FCQ4[i,j,j,j]**2*(f[i] + 12*f[j]**2 + 6*f[j]*(f[i]*f[j] + f[i] + f[j]**2) + 7*f[j] + 1)/(4*(w[i] + w[j]))#4*4*2
+                    D16 +=  FCQ4[i,j,j,j]**2*(f[i] + 6*f[j]*(f[i]*f[j] + f[i] - f[j]**2 - f[j]) - f[j])/(4*(w[i] - w[j]))#4*4*2
                     D05 += -FCQ4[i,j,j,j]*FCQ4[i,i,i,j]*(2*f[i]**2 + 4*f[i]*f[j]*(f[i] + f[j]) + 8*f[i]*f[j] + 3*f[i] + 2*f[j]**2 + 3*f[j] + 1)/(2*(w[i] + w[j]))#4*4*2
                     D06 +=  FCQ4[i,j,j,j]*FCQ4[i,i,i,j]*(2*f[i]**2 + 4*f[i]*f[j]*(f[i] - f[j]) + f[i] - 2*f[j]**2 - f[j])/(2*(w[i] - w[j]))# 4*4*2
-                    D11 += -FCQ4[i,j,j,j]**2*(f[i] + f[j]**3 + 3*f[j]*(f[i]*f[j] + f[i] + f[j] + 1) + 1)/(12*(w[i] + 3*w[j]))#4*4
-                    D12 +=  FCQ4[i,j,j,j]**2*(3*f[i]*f[j]*(f[j] + 1) + f[i] - f[j]**3)/(12*(w[i] - 3*w[j]))#4*4
+                    D11 += -2*FCQ4[i,j,j,j]**2*(f[i] + f[j]**3 + 3*f[j]*(f[i]*f[j] + f[i] + f[j] + 1) + 1)/(12*(w[i] + 3*w[j]))#4*4*2
+                    D12 +=  2*FCQ4[i,j,j,j]**2*(3*f[i]*f[j]*(f[j] + 1) + f[i] - f[j]**3)/(12*(w[i] - 3*w[j]))#4*4*2
                 for k in range(nmode):
                     if( k!= i and k!=j and j!=i):
                         S07 += -FCQ3[i,k,k]*FCQ3[i,j,j]*(4*f[j]*f[k] + 2*f[j] + 2*f[k] + 1)/(4*w[i])
@@ -107,16 +107,16 @@ class Numerical:
                         T02 +=  FCQ3[i,j,k]**2*(-f[i]*f[j] + f[i]*f[k] - f[j]*f[k] - f[j])/(w[i] - w[j] + w[k])/6
                         T06 +=  FCQ3[i,j,k]**2*(f[i]*f[j] + f[i]*f[k] + f[i] - f[j]*f[k])/(w[i] - w[j] - w[k])/6
 
-                        D10 +=  FCQ4[i,j,k,k]**2*(f[i] - f[j] + 8*f[k]*(f[i]*f[k] + f[i] - f[j]*f[k] - f[j]))/(16*(w[i] - w[j]))*2#6 * 6???XXX 
-                        D02 += -FCQ4[i,j,k,k]**2*(f[i] + f[j] + 8*f[k]*(f[i]*f[k] + f[i] + f[j]*f[k] + f[j] + f[k] + 1) + 1)/(16*(w[i] + w[j]))*2#6 * 6 ???
+                        D10 +=  FCQ4[i,j,k,k]**2*(f[i] - f[j] + 8*f[k]*(f[i]*f[k] + f[i] - f[j]*f[k] - f[j]))/(8*(w[i] - w[j]))#6 * 6???XXX 
+                        D02 += -FCQ4[i,j,k,k]**2*(f[i] + f[j] + 8*f[k]*(f[i]*f[k] + f[i] + f[j]*f[k] + f[j] + f[k] + 1) + 1)/(8*(w[i] + w[j]))#6 * 6 ???
                         D03 += -FCQ4[i,j,k,k]*FCQ4[i,j,j,j]*(2*f[i]*f[j] + 2*f[i]*f[k] + f[i] + 2*f[j]**2 + 4*f[j]*f[k]*(f[i] + f[j]) + 6*f[j]*f[k] + 3*f[j] + 2*f[k] + 1)/(4*(w[i] + w[j]))#6*4*2
                         D04 +=  FCQ4[i,j,k,k]*FCQ4[i,j,j,j]*(2*f[i]*f[j] + 2*f[i]*f[k] + f[i] - 2*f[j]**2 + 4*f[j]*f[k]*(f[i] - f[j]) - 2*f[j]*f[k] - f[j])/(4*(w[i] - w[j]))#6*4*2
 
                         S09 += -FCQ4[i,i,j,j]*FCQ4[i,i,k,k]*(8*f[i]*f[k]*f[j] + 4*f[i]*f[k] + 4*f[i]*f[j] + 2*f[i] + 4*f[k]*f[j] + 2*f[k] + 2*f[j] + 1)/(16*w[i])#6*6
-                        T03 += -FCQ4[i,j,k,k]**2*(f[i]*f[j] + f[i]*f[k]**2 + f[i] + f[j]*f[k]**2 + f[j] + f[k]**2 + 2*f[k]*(f[i]*f[j] + f[i] + f[j] + 1) + 1)/(8*(w[i] + w[j] + 2*w[k]))# 6*6 
-                        T07 +=  FCQ4[i,j,k,k]**2*(2*f[i]*f[j]*f[k] + f[i]*f[j] - f[i]*f[k]**2 - f[j]*f[k]**2 - f[k]**2)/(8*(w[i] + w[j] - 2*w[k]))
-                        T04 += -FCQ4[i,j,k,k]**2*(f[i]*f[j] - f[i]*f[k]**2 + f[j]*f[k]**2 + 2*f[j]*f[k]*(f[i] + 1) + f[j])/(8*(w[i] - w[j] + 2*w[k]))
-                        T08 +=  FCQ4[i,j,k,k]**2*(f[i]*f[j] + f[i]*f[k]**2 + 2*f[i]*f[k]*(f[j] + 1) + f[i] - f[j]*f[k]**2)/(8*(w[i] - w[j] - 2*w[k]))
+                        T03 += -2*FCQ4[i,j,k,k]**2*(f[i]*f[j] + f[i]*f[k]**2 + f[i] + f[j]*f[k]**2 + f[j] + f[k]**2 + 2*f[k]*(f[i]*f[j] + f[i] + f[j] + 1) + 1)/(8*(w[i] + w[j] + 2*w[k]))# 6*6 *2 can't believe this 2
+                        T07 +=  2*FCQ4[i,j,k,k]**2*(2*f[i]*f[j]*f[k] + f[i]*f[j] - f[i]*f[k]**2 - f[j]*f[k]**2 - f[k]**2)/(8*(w[i] + w[j] - 2*w[k]))
+                        T04 += -2*FCQ4[i,j,k,k]**2*(f[i]*f[j] - f[i]*f[k]**2 + f[j]*f[k]**2 + 2*f[j]*f[k]*(f[i] + 1) + f[j])/(8*(w[i] - w[j] + 2*w[k]))
+                        T08 +=  2*FCQ4[i,j,k,k]**2*(f[i]*f[j] + f[i]*f[k]**2 + 2*f[i]*f[k]*(f[j] + 1) + f[i] - f[j]*f[k]**2)/(8*(w[i] - w[j] - 2*w[k]))
                     for l in range(nmode):
                         if(l!=i and l!=j and l!=k and i!=j and i!=k and j!=k):
                             D07 += -FCQ4[i,j,l,l]*FCQ4[i,j,k,k]*(2*f[i]*f[k] + 2*f[i]*f[l] + f[i] + 2*f[j]*f[k] + 2*f[j]*f[l] + f[j] + 4*f[k]*f[l]*(f[i] + f[j] + 1) + 2*f[k] + 2*f[l] + 1)/(8*(w[i] + w[j]))
@@ -142,7 +142,13 @@ class Numerical:
         #print(D12+D11)
         #print(D15+D16)
         #print(D03+D04)
-        print(D05+D06)
+        #print(D05+D06)
+        #print(S04+D13+D14)
+        #print(D02+D10+T03+T04+T07+T08)
+        #print(T03+T04+T07+T08)
+        #print(D02+D10)
+        #print(D11+D12+D15+D16)
+        #print(D07+D08)
 
 
     def GFnumeric(self,w,f,FCQ3,FCQ4,nmode):
@@ -164,14 +170,15 @@ class Numerical:
         GP6 = 0 #iijj iiii 2BD
         GP3 = 0 #ijii ijjj 2BN
 
-        GP2 = 0 #ijkk ijjj 2B
-        GP7 = 0 #iikk iijj 2B
+        GP2 = 0 #ijkk ijjj 2BN
+        GP7 = 0 #iikk iijj 2BD
         GP4 = 0 #iiii^2 2B 2D
         GP5 = 0 #iijj^2 2B 2D
         GP8 = 0 #ijjj^2 2B 2D
         GP9 = 0 #ijkk^2 2B 2D
         test4th1 = 0
         test4th2 = 0
+        count= 0
         for i in range(nmode):
             for j in range(nmode):
                 #A1 += FCQ4[i,i,j,j]*(2*f[i]+1)*(2*f[j]+1)/8
@@ -180,6 +187,18 @@ class Numerical:
                     GP6t2 = [i,i,k,k]
                     if ((len(set(GP6t1))==2 and len(set(GP6t2))==1) or(len(set(GP6t1))==1 and len(set(GP6t2))==2)):
                         GP6+= - FCQ4[i,i,j,j]*FCQ4[i,i,k,k]*(2*f[j]+1)*(2*f[k]+1)*(f[i]+1/2)/w[i]/8
+                    GP7t1 = [i,i,j,j]
+                    GP7t2 = [i,i,k,k]
+                    if ((len(set(GP7t1))==2 and len(set(GP7t2))==2) and j!=k):
+                        GP7 += - FCQ4[i,i,j,j]*FCQ4[i,i,k,k]*(2*f[j]+1)*(2*f[k]+1)*(f[i]+1/2)/w[i]/8
+                    GP4t1 = [i,i,j,j]
+                    GP4t2 = [i,i,k,k]
+                    if ((len(set(GP4t1))==1 and len(set(GP4t2))==1)):
+                        GP4+= -FCQ4[i,i,j,j]*FCQ4[i,i,k,k]*(2*f[j]+1)*(2*f[k]+1)*(f[i]+1/2)/w[i]/8
+                    GP5t1 = [i,i,j,j]
+                    GP5t2 = [i,i,k,k]
+                    if ((len(set(GP5t1))==2 and len(set(GP5t2))==2) and j==k):
+                        GP5 += - FCQ4[i,i,j,j]*FCQ4[i,i,k,k]*(2*f[j]+1)*(2*f[k]+1)*(f[i]+1/2)/w[i]/8
                     B2D += -FCQ4[i,i,j,j]*FCQ4[i,i,k,k]*(2*f[j]+1)*(2*f[k]+1)*(f[i]+1/2)/w[i]/8
                     #A2 += -FCQ3[i,j,j]*FCQ3[i,k,k]*(2*f[j]+1)*(2*f[k]+1)/w[i]/4     
                     #C2 += -FCQ3[i,j,k]**2*((f[i]*f[j]+f[j]*f[k]+f[i]*f[k]+f[i]+f[j]+f[k]+1)/(w[i]+w[j]+w[k])-(f[i]*f[j]+f[j]*f[k]-f[i]*f[k]+f[j])/(w[j]-w[i]-w[k])-(f[i]*f[j]-f[k]*f[j]-f[i]*f[k]-f[k])/(w[j]+w[i]-w[k])-(f[k]*f[j]-f[i]*f[j]-f[i]*f[k]-f[i])/(w[j]-w[i]+w[k]))/6
@@ -189,36 +208,104 @@ class Numerical:
                             GP3t2 = [i,j,l,l]
                             if (len(set(GP3t1))==2 and len(set(GP3t2))==2 and k!=l ):
                                 GP3 += -FCQ4[i,j,k,k]*FCQ4[i,j,l,l]*(2*f[k]+1)*(2*f[l]+1)*(w[i]*(2*f[j]+1) - w[j]*(2*f[i]+1))/(w[i]**2-w[j]**2)/8
+                            GP2t1 = [i,j,k,k]
+                            GP2t2 = [i,j,l,l]
+                            if ((len(set(GP2t1))==3 and len(set(GP2t2))==2) or (len(set(GP2t1))==2 and len(set(GP2t2))==3)):
+                                GP2 += -FCQ4[i,j,k,k]*FCQ4[i,j,l,l]*(2*f[k]+1)*(2*f[l]+1)*(w[i]*(2*f[j]+1) - w[j]*(2*f[i]+1))/(w[i]**2-w[j]**2)/8
+                            GP9t1 = [i,j,k,k]
+                            GP9t2 = [i,j,l,l]
+                            if (len(set(GP9t1))==3 and len(set(GP9t2))==3 and k==l):
+                                GP9 += -FCQ4[i,j,k,k]*FCQ4[i,j,l,l]*(2*f[k]+1)*(2*f[l]+1)*(w[i]*(2*f[j]+1) - w[j]*(2*f[i]+1))/(w[i]**2-w[j]**2)/8
+                            GP8t1 = [i,j,k,k]
+                            GP8t2 = [i,j,l,l]
+                            if (len(set(GP8t1))==2 and len(set(GP8t2))==2 and k==l):
+                                #count+=1
+                                GP8 += -FCQ4[i,j,k,k]*FCQ4[i,j,l,l]*(2*f[k]+1)*(2*f[l]+1)*(w[i]*(2*f[j]+1) - w[j]*(2*f[i]+1))/(w[i]**2-w[j]**2)/8
                             B2N+= -FCQ4[i,j,k,k]*FCQ4[i,j,l,l]*(2*f[k]+1)*(2*f[l]+1)*(w[i]*(2*f[j]+1) - w[j]*(2*f[i]+1))/(w[i]**2-w[j]**2)/8
+                        GP4t3 = [i,j,k,l]
+                        if ( len(set(GP4t3))==1):
+                            GP4 += -FCQ4[i,j,k,l]**2*(f[i]*f[k]*(f[j]+f[l]+1)+f[i]*(f[l]+1)*(f[j]+1)-f[k]*f[j]*f[l])/(-w[i]+w[j]+w[k]+w[l])/24
+                            GP4 += -FCQ4[i,j,k,l]**2*(f[l]*f[k]*(f[j]+f[i]+1)+f[l]*(f[i]+1)*(f[j]+1)-f[i]*f[j]*f[k])/(w[i]+w[j]+w[k]-w[l])/24
+                            GP4 += -FCQ4[i,j,k,l]**2*((f[k]+1)*(f[i]+1)*(f[l]+1)*(f[j]+1)-f[i]*f[j]*f[k]*f[l])/(w[i]+w[j]+w[k]+w[l])/24
+                            GP4 += -FCQ4[i,j,k,l]**2*(f[k]*f[j]*(f[i]+f[l]+1)+f[j]*(f[i]+1)*(f[l]+1)-f[i]*f[k]*f[l])/(w[i]-w[j]+w[k]+w[l])/24
+                            GP4 += -FCQ4[i,j,k,l]**2*(f[k]*(f[i]+1)*(f[j]+1)*(f[l]+1)-f[i]*f[j]*f[l]*(f[k]+1))/(w[i]+w[j]-w[k]+w[l])/24
+                            if (-w[i] + w[j] - w[k] + w[l]!= 0 ):
+                                GP4 += -FCQ4[i,j,k,l]**2*(f[i]*f[k]*(f[j]+f[l]+1)-f[j]*f[l]*(f[i]+f[k]+1))/(-w[i]+w[j]-w[k]+w[l])/24
+                            if (-w[i] + w[j] + w[k] - w[l]!= 0 ):
+                                GP4 += -FCQ4[i,j,k,l]**2*(f[i]*f[l]*(f[k]+f[j]+1)-f[j]*f[k]*(f[i]+f[l]+1))/(-w[i]+w[j]+w[k]-w[l])/24
+                            if (w[i] + w[j] - w[k] - w[l]!= 0 ):
+                                GP4 += -FCQ4[i,j,k,l]**2*(f[k]*f[l]*(f[j]+f[i]+1)-f[i]*f[j]*(f[l]+f[k]+1))/(w[i]+w[j]-w[k]-w[l])/24
+                        GP5t3 = [i,j,k,l]
+                        if ( len(set(GP5t3))==2 and GP5t3.count(list(set(GP5t3))[0])==2 and GP5t3.count(list(set(GP5t3))[1])==2):#need to exclude ijjj -> set would be 2 too
+                            GP5 += -FCQ4[i,j,k,l]**2*(f[i]*f[k]*(f[j]+f[l]+1)+f[i]*(f[l]+1)*(f[j]+1)-f[k]*f[j]*f[l])/(-w[i]+w[j]+w[k]+w[l])/24
+                            GP5 += -FCQ4[i,j,k,l]**2*(f[l]*f[k]*(f[j]+f[i]+1)+f[l]*(f[i]+1)*(f[j]+1)-f[i]*f[j]*f[k])/(w[i]+w[j]+w[k]-w[l])/24
+                            GP5 += -FCQ4[i,j,k,l]**2*((f[k]+1)*(f[i]+1)*(f[l]+1)*(f[j]+1)-f[i]*f[j]*f[k]*f[l])/(w[i]+w[j]+w[k]+w[l])/24
+                            GP5 += -FCQ4[i,j,k,l]**2*(f[k]*f[j]*(f[i]+f[l]+1)+f[j]*(f[i]+1)*(f[l]+1)-f[i]*f[k]*f[l])/(w[i]-w[j]+w[k]+w[l])/24
+                            GP5 += -FCQ4[i,j,k,l]**2*(f[k]*(f[i]+1)*(f[j]+1)*(f[l]+1)-f[i]*f[j]*f[l]*(f[k]+1))/(w[i]+w[j]-w[k]+w[l])/24
+                            if (-w[i] + w[j] - w[k] + w[l]!= 0 ):
+                                GP5 += -FCQ4[i,j,k,l]**2*(f[i]*f[k]*(f[j]+f[l]+1)-f[j]*f[l]*(f[i]+f[k]+1))/(-w[i]+w[j]-w[k]+w[l])/24
+                            if (-w[i] + w[j] + w[k] - w[l]!= 0 ):
+                                GP5 += -FCQ4[i,j,k,l]**2*(f[i]*f[l]*(f[k]+f[j]+1)-f[j]*f[k]*(f[i]+f[l]+1))/(-w[i]+w[j]+w[k]-w[l])/24
+                            if (w[i] + w[j] - w[k] - w[l]!= 0 ):
+                                GP5 += -FCQ4[i,j,k,l]**2*(f[k]*f[l]*(f[j]+f[i]+1)-f[i]*f[j]*(f[l]+f[k]+1))/(w[i]+w[j]-w[k]-w[l])/24
+
+                        GP9t3 = [i,j,k,l]
+                        if ( len(set(GP9t3))==3):
+                            GP9 += -FCQ4[i,j,k,l]**2*(f[i]*f[k]*(f[j]+f[l]+1)+f[i]*(f[l]+1)*(f[j]+1)-f[k]*f[j]*f[l])/(-w[i]+w[j]+w[k]+w[l])/24
+                            GP9 += -FCQ4[i,j,k,l]**2*(f[l]*f[k]*(f[j]+f[i]+1)+f[l]*(f[i]+1)*(f[j]+1)-f[i]*f[j]*f[k])/(w[i]+w[j]+w[k]-w[l])/24
+                            GP9 += -FCQ4[i,j,k,l]**2*((f[k]+1)*(f[i]+1)*(f[l]+1)*(f[j]+1)-f[i]*f[j]*f[k]*f[l])/(w[i]+w[j]+w[k]+w[l])/24
+                            GP9 += -FCQ4[i,j,k,l]**2*(f[k]*f[j]*(f[i]+f[l]+1)+f[j]*(f[i]+1)*(f[l]+1)-f[i]*f[k]*f[l])/(w[i]-w[j]+w[k]+w[l])/24
+                            GP9 += -FCQ4[i,j,k,l]**2*(f[k]*(f[i]+1)*(f[j]+1)*(f[l]+1)-f[i]*f[j]*f[l]*(f[k]+1))/(w[i]+w[j]-w[k]+w[l])/24
+                            if (-w[i] + w[j] - w[k] + w[l]!= 0 ):
+                                GP9 += -FCQ4[i,j,k,l]**2*(f[i]*f[k]*(f[j]+f[l]+1)-f[j]*f[l]*(f[i]+f[k]+1))/(-w[i]+w[j]-w[k]+w[l])/24
+                            if (-w[i] + w[j] + w[k] - w[l]!= 0 ):
+                                GP9 += -FCQ4[i,j,k,l]**2*(f[i]*f[l]*(f[k]+f[j]+1)-f[j]*f[k]*(f[i]+f[l]+1))/(-w[i]+w[j]+w[k]-w[l])/24
+                            if (w[i] + w[j] - w[k] - w[l]!= 0 ):
+                                GP9 += -FCQ4[i,j,k,l]**2*(f[k]*f[l]*(f[j]+f[i]+1)-f[i]*f[j]*(f[l]+f[k]+1))/(w[i]+w[j]-w[k]-w[l])/24
+                        GP8t3 = [i,j,k,l]
+                        if ( len(set(GP8t3))==2 and GP8t3.count(list(set(GP8t3))[0])!=2 and GP8t3.count(list(set(GP8t3))[1])!=2):
+                            #count+=1
+                            GP8 += -FCQ4[i,j,k,l]**2*(f[i]*f[k]*(f[j]+f[l]+1)+f[i]*(f[l]+1)*(f[j]+1)-f[k]*f[j]*f[l])/(-w[i]+w[j]+w[k]+w[l])/24
+                            GP8 += -FCQ4[i,j,k,l]**2*(f[l]*f[k]*(f[j]+f[i]+1)+f[l]*(f[i]+1)*(f[j]+1)-f[i]*f[j]*f[k])/(w[i]+w[j]+w[k]-w[l])/24
+                            GP8 += -FCQ4[i,j,k,l]**2*((f[k]+1)*(f[i]+1)*(f[l]+1)*(f[j]+1)-f[i]*f[j]*f[k]*f[l])/(w[i]+w[j]+w[k]+w[l])/24
+                            GP8 += -FCQ4[i,j,k,l]**2*(f[k]*f[j]*(f[i]+f[l]+1)+f[j]*(f[i]+1)*(f[l]+1)-f[i]*f[k]*f[l])/(w[i]-w[j]+w[k]+w[l])/24
+                            GP8 += -FCQ4[i,j,k,l]**2*(f[k]*(f[i]+1)*(f[j]+1)*(f[l]+1)-f[i]*f[j]*f[l]*(f[k]+1))/(w[i]+w[j]-w[k]+w[l])/24
+                            if (-w[i] + w[j] - w[k] + w[l]!= 0 ):
+                                GP8 += -FCQ4[i,j,k,l]**2*(f[i]*f[k]*(f[j]+f[l]+1)-f[j]*f[l]*(f[i]+f[k]+1))/(-w[i]+w[j]-w[k]+w[l])/24
+                            if (-w[i] + w[j] + w[k] - w[l]!= 0 ):
+                                GP8 += -FCQ4[i,j,k,l]**2*(f[i]*f[l]*(f[k]+f[j]+1)-f[j]*f[k]*(f[i]+f[l]+1))/(-w[i]+w[j]+w[k]-w[l])/24
+                            if (w[i] + w[j] - w[k] - w[l]!= 0 ):
+                                GP8 += -FCQ4[i,j,k,l]**2*(f[k]*f[l]*(f[j]+f[i]+1)-f[i]*f[j]*(f[l]+f[k]+1))/(w[i]+w[j]-w[k]-w[l])/24
                         D1 += -FCQ4[i,j,k,l]**2*(f[i]*f[k]*(f[j]+f[l]+1)+f[i]*(f[l]+1)*(f[j]+1)-f[k]*f[j]*f[l])/(-w[i]+w[j]+w[k]+w[l])/24
-                        if (-w[i] + w[j] + w[k] - w[l]!= 0 ):
-                            D2 += -FCQ4[i,j,k,l]**2*(f[i]*f[l]*(f[k]+f[j]+1)-f[j]*f[k]*(f[i]+f[l]+1))/(-w[i]+w[j]+w[k]-w[l])/24
                         D3 += -FCQ4[i,j,k,l]**2*(f[l]*f[k]*(f[j]+f[i]+1)+f[l]*(f[i]+1)*(f[j]+1)-f[i]*f[j]*f[k])/(w[i]+w[j]+w[k]-w[l])/24
                         D4 += -FCQ4[i,j,k,l]**2*((f[k]+1)*(f[i]+1)*(f[l]+1)*(f[j]+1)-f[i]*f[j]*f[k]*f[l])/(w[i]+w[j]+w[k]+w[l])/24
-                        if (w[i] + w[j] - w[k] - w[l]!= 0 ):
-                            D5 += -FCQ4[i,j,k,l]**2*(f[k]*f[l]*(f[j]+f[i]+1)-f[i]*f[j]*(f[l]+f[k]+1))/(w[i]+w[j]-w[k]-w[l])/24
                         D6 += -FCQ4[i,j,k,l]**2*(f[k]*f[j]*(f[i]+f[l]+1)+f[j]*(f[i]+1)*(f[l]+1)-f[i]*f[k]*f[l])/(w[i]-w[j]+w[k]+w[l])/24
+                        D8 += -FCQ4[i,j,k,l]**2*(f[k]*(f[i]+1)*(f[j]+1)*(f[l]+1)-f[i]*f[j]*f[l]*(f[k]+1))/(w[i]+w[j]-w[k]+w[l])/24
                         if (-w[i] + w[j] - w[k] + w[l]!= 0 ):
                             D7 += -FCQ4[i,j,k,l]**2*(f[i]*f[k]*(f[j]+f[l]+1)-f[j]*f[l]*(f[i]+f[k]+1))/(-w[i]+w[j]-w[k]+w[l])/24
-                        D8 += -FCQ4[i,j,k,l]**2*(f[k]*(f[i]+1)*(f[j]+1)*(f[l]+1)-f[i]*f[j]*f[l]*(f[k]+1))/(w[i]+w[j]-w[k]+w[l])/24
+                        if (-w[i] + w[j] + w[k] - w[l]!= 0 ):
+                            D2 += -FCQ4[i,j,k,l]**2*(f[i]*f[l]*(f[k]+f[j]+1)-f[j]*f[k]*(f[i]+f[l]+1))/(-w[i]+w[j]+w[k]-w[l])/24
+                        if (w[i] + w[j] - w[k] - w[l]!= 0 ):
+                            D5 += -FCQ4[i,j,k,l]**2*(f[k]*f[l]*(f[j]+f[i]+1)-f[i]*f[j]*(f[l]+f[k]+1))/(w[i]+w[j]-w[k]-w[l])/24
         GFresult3rd = A2 + C2 
         GFresult4th = B2N +B2D +D1 +D2 +D3 +D4 +D5 +D6 +D7 +D8 
         print("GF")
-        print("3rd....")
+        #print("3rd....")
         #print(GFresult3rd)
         print("4th....")
         #print(GFresult4th)
-        #print(B2D+ 3*(D1 +D2 +D3 +D6 +D7 +D8)+6*(D4 +D5) )2.13 2.14 1.04
+        #print(B2D+ 3*(D1 +D2 +D3 +D6 +D7 +D8)+3*(D4 +D5) )#2.13 2.14 1.04
         #print(6*(D4+D5)) 2.13 2.14This six I am not sure. If there are no other bugs then should be this one 
         #print(B2D+3*(D3+D8+D1+D6)) #1.04 
         #print(B2N + 6*(D2+D3+D7+D8)) #2.02 2.10
-        #print(2*(D1+D4)) 2.15 16
-        #print(B2N + 2*(D2+D3+D5+D6+D7+D8)) 2.11 2.12
+        #print(6*(D5+D6+D4+D1))# 3.03 04 07 08
+        #print(2*(D1+D4)) #2.11 12
+        #print(B2N + 2*(D2+D3+D5+D6+D7+D8))# 2.15 2.16
         #print(B2N*2) 2.03 2.04
         #print(B2N*4) #2.05 2.06
         #----------------
-        print(GP3)
-
+        #print(count)
+        #print(GP2+GP3+GP4+GP5+GP6+GP7+GP8)
 
     def readSindoPES(self,filepath,nmode):
         w_omega = np.zeros(nmode)
